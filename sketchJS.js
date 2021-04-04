cab627b7f8e651af6c43607c40a0a597be3d32b1
Irad Ya'acoby <iradyaacoby@gmail.com>
2021-04-04 17:20:50 +0300

    updated

--- sketchJS.js
+++
@@ -1,4 +0,0 @@
-alert ("i'm JavaScript!");
-alert ("oops! i said it twice using two methods! one with an internal script tag and four more alerts using an external js file!");
-alert ("Oh yess!");
-alert ("i'm the great developer!");
