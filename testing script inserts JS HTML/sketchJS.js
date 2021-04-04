f3ab56db029b8bb35fbb36652a450889eb26b532
Irad Ya'acoby <iradyaacoby@gmail.com>
2021-04-04 15:30:01 +0300

    uploaded the first files

---
+++ testing script inserts JS HTML/sketchJS.js
@@ -0,0 +1,4 @@
+alert ("i'm JavaScript!");
+alert ("oops! i said it twice using two methods! one with an internal script tag and four more alerts using an external js file!");
+alert ("Oh yess!");
+alert ("i'm the great developer!");
